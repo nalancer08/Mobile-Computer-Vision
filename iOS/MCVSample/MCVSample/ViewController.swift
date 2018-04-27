498e5814d3c553cdae969c134285c3eef15700b7
Strategic Impact Solutions 1 <strategicimpactsolutions1@Strategics-MacBook-Pro.local>
2018-04-27 12:08:04 -0500

    Added base ios framework and demo

---
+++ iOS/MCVSample/MCVSample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MCVSample
+//
+//  Created by Strategic Impact Solutions 1 on 27/04/18.
+//  Copyright © 2018 App Builders. All rights reserved.
+//
+
+import UIKit
+import MobileComputerVision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
